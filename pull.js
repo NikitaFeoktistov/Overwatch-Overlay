bc7351919b8cc202fa7ce289e5f6d0882d91bd45
Amer Wali <amerwali723@gmail.com>
2023-01-03 13:14:10 -0600

    daf

--- pull.js
+++ pull.js
@@ -1,10 +1,12 @@
+/**
+import XMLHttpRequest from 'xhr2';
 
 
 function returnPlayerData() {
     // https://levelup.gitconnected.com/all-possible-ways-of-making-an-api-call-in-plain-javascript-c0dee3c11b8b
     // code to get JSON data 
     let request = new XMLHttpRequest();
-    request.open("GET", "https://best-overwatch-api.herokuapp.com/player/pc/us/GamersCCCP-1569")
+    request.open("GET", 'https://randomuser.me/api/')
     request.send();
     request.onload = () => {
         if (request.status === 200){
@@ -12,5 +14,32 @@ function returnPlayerData() {
         } else {
             console.log(`error ${request.status} ${request.statusText}`);
      }
+    return request.responseText;
     }
 }
+
+console.log(returnPlayerData());
+
+*/
+
+
+
+import fetch from 'node-fetch';
+
+
+
+async function returnPlayerData() {
+    let response = await fetch('https://randomuser.me/api/');
+    if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+    let data = await response.json();
+    return data;
+}
+
+returnPlayerData().then(data => console.log(data));
+
+
+const obj = returnPlayerData();
+console.log(obj);
+console.log("hi");
